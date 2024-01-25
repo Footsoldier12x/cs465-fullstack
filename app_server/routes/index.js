7c7f13673785ef6486c1431080220563c239137c
Footsoldier12x <toxic_corp_tx@hotmail.com>
2024-01-25 11:07:19 +0100

    Correction to controllers/main.js and changed var to const in routes folder

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const ctlrMain = require('../controllers/main');
 
-/* GET home page. */
+/* GET home page */
 router.get('/', ctlrMain.index); 
 
 module.exports = router;
